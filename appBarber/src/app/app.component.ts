4122650463694bdeccd4cdb43f1eaefc65f5f119
Klemerson01 <kleminho.rodrigues0102@gmail.com>
2019-10-19 23:14:47 -0300

    page cadastro no menu

--- appBarber/src/app/app.component.ts
+++ appBarber/src/app/app.component.ts
@@ -20,6 +20,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Cadastro',
+      url: '/cadastro',
+      icon: 'cadastro'
     }
   ];
 
